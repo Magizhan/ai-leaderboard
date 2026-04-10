832a770bc6e13427444da36fa0d6c6f87ce4e5c8
piyushkumar-1 <pkkapoor98@gmail.com>
2026-04-10 15:54:58 +0530

    increasing import data default uplaod limit from 100kb to 5mb

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const env = {
 };
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 // Serve static files from public/
 app.use(express.static(path.join(__dirname, '..', 'public')));
